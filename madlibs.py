b952592aee45aead7e5bbd4c84064315c3d3d822
ablades <audaris@gmail.com>
2019-08-29 17:21:03 -0700

    added first iteration of story

--- madlibs.py
+++ madlibs.py
@@ -16,3 +16,20 @@ def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
+
+#Show line of story for user.
+#Ask user for input
+#Show next line ect
+#At end show completed story with colors for each input
+
+"Once upon a time there was a man. \n"
+"His name was Barry Allen. \n"
+"Barry was a hard working man and often stayed late for work. \n"
+"One evening on a dark stormy night as Barry finished work. \n"
+"Lightning struck and knocked barry into a cocktail of chemicals. \n"
+"Barry remained in a coma for 4 months. \n"
+"Once Barry had woke he realized something had changed. \n"
+"He was faster, stronger, quicker, and more agile. \n"
+"Barry would go on to be known as The Flash. \n"
+
+
